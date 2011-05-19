3a755ad9b89a06e9726d126f664a6e431f417f9d
francis <francis@flourish.org>
2011-05-19 14:32:17 +0100

    Machine name change

---
+++ uml/controller/exec.php
@@ -0,0 +1,83 @@
+#!/usr/bin/php5
+<?php
+
+// set the include paths to scraperlibs from an environment variable (what can be done automatically for python and ruby)
+foreach (split(':', getenv('PHPPATH')) as $dir)
+    ini_set('include_path',  ini_get('include_path') . PATH_SEPARATOR . $dir) ;
+
+$logfd = fopen("/proc/self/fd/3", "w") ;
+
+require_once 'scraperwiki.php';
+require_once 'scraperwiki/datastore.php';
+require_once 'scraperwiki/stacktrace.php';
+
+ob_implicit_flush(true);
+
+$script = null;
+$datastore = null;
+
+for ($idx = 1; $idx < count($argv); $idx += 1)
+{
+   $arg  = $argv[$idx] ;
+
+   if (substr ($arg, 0,  9) == '--script=')
+      $script = substr ($arg, 9);
+   if (substr ($arg, 0, 5) == '--ds=')
+      $datastore = substr($arg, 5);
+   if (substr($arg, 0, 6) == '--gid=')
+   {
+      $gid = substr($arg,  6);
+      posix_setgid($gid);
+      posix_setegid($gid);
+   }
+   if (substr ($arg, 0, 6) == '--uid=')
+   {
+      $uid = substr($arg, 6);
+      posix_setuid($uid);
+      posix_seteuid($uid);
+   }
+}
+
+
+$dsinfo = split (':', $datastore) ;
+SW_DataStoreClass::create ($dsinfo[0], $dsinfo[1]) ;
+
+// the following might be the only way to intercept syntax errors
+//$errors = array(); 
+//parsekit_compile_file($script, $errors); 
+
+// refer to http://php.net/manual/en/function.set-error-handler.php
+function errorHandler($errno, $errstr, $errfile, $errline)
+{
+    global $script; 
+    $etb = errorParser($errno, $errstr, $errfile, $errline, $script); 
+    scraperwiki::sw_dumpMessage($etb); 
+    return true; 
+}
+
+set_error_handler("errorHandler", E_ALL & ~E_NOTICE);  // this is for errors, not exceptions (eg 1/0)
+
+/*
+    Can't get this to work - the exception raised inside the signal handler
+    just makes the script fail silently. This isn't the end of the world,
+    as higher level code at least now says it was SIGXCPU that killed it.
+
+    Would be nice to get the stack trace though, like in Ruby/Python!
+
+function sigXCPU($signum) {
+    throw new Exception("ScraperWiki CPU time exceeded");
+}
+pcntl_signal(SIGXCPU, "sigXCPU"); */
+
+try
+{
+    // works also as include or eval.  However no way to trap syntax errors
+    require  $script  ;
+}
+catch(Exception $e)
+{
+    $etb = exceptionHandler($e, $script);
+    //print_r($etb); 
+    scraperwiki::sw_dumpMessage($etb); 
+}
+?>
