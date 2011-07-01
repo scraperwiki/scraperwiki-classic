d8175a96afec3dc590e3423c9a410277d223d7fe
rossjones <rossjones@localhost>
2011-07-01 11:40:24 +0100

    Copied across and changed (using optparse) the arg processing from the old proxy.

--- uml/httpproxy/httpproxy.py
+++ uml/httpproxy/httpproxy.py
@@ -1,9 +1,14 @@
-import sys
+import os,sys
+
+import ConfigParser
+
+import memcache
 
 from twisted.web import proxy, http
 from twisted.python import log
 
-log.startLogging(sys.stdout)
+
+USAGE = " [--uid=#] [--gid=#] [--allowAll] [--varDir=dir] [--config=file] [--useCache] [--mode=H|S|P]"
 
 class ScraperProxyClient(proxy.ProxyClient):
 
@@ -56,17 +61,60 @@ class ScraperProxyFactory(http.HTTPFactory):
     def buildProtocol(self, addr):
         protocol = ScraperProxy()
         return protocol
-    
+        
+        
         
 if __name__ == '__main__':
     from twisted.internet import reactor
+    from optparse import OptionParser
+    
+    parser = OptionParser(usage=USAGE)
+    parser.add_option("-u", "--uid", type='int', dest="uid",
+                       help="Run as supplied uid")
+    parser.add_option("-g", "--gid", type='int', dest="gid",
+                       help="Run as supplied gif")
+    parser.add_option("-v", "--varDir", dest="varDir",
+                        help="Specify var directory")
+    parser.add_option("-c", "--config", dest="config",
+                        help="Specify config file")
+                        
+    parser.add_option("-x", "--useCache", action="store_false", dest="useCache", default=False,
+                  help="Force use of cache")
+    parser.add_option("-a", "--allowAll", action="store_false", dest="allowAll", default=False,
+                  help="Allow all urls")
+    
+    (options, args) = parser.parse_args()
+
+    # Following only needed when not using twistd
+    log.startLogging(sys.stdout)
+
+    # Set uid and gid if they were provided as command line arguments            
+    if options.uid:
+        log.msg('Switching to UID %s' % options.uid)
+        os.setreuid (options.uid, options.uid)
+
+    if options.gid:
+        log.msg('Switching to GID %s' % options.gid)
+        os.setregid (options.gid, options.gid)
+
+    # Log startup arguments
+    log.msg("Starting with var directory: %s" % options.varDir )
+    log.msg("Reading config file: %s" % (options.config or 'uml.cfg',) )
+    log.msg("Force cache: %s" % options.useCache )
+    log.msg("Allow all: %s" % options.allowAll )    
+
+    # Load config file
+    config = ConfigParser.ConfigParser()
+    try:
+        config.readfp( open( options.config or 'uml.cfg') )
+    except IOError, ioe: 
+        log.err(ioe, "Trying to read log file")
+        sys.exit()
+
+    cache_hosts = config.get(varName, 'cache')
+    if cache_hosts:
+        cache_client = memcache.Client( cache_hosts.split(',') )        
+    
     px = ScraperProxyFactory()
     reactor.listenTCP(9000, px)
     reactor.run()
-
-    
-    
-    
-    
-    
-    
