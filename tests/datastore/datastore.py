99a4880979cc7ed3e5a0fa2078d9a745cc6a7f9a
francis <francis@flourish.org>
2011-11-22 16:08:39 +0000

    Log where exception was

---
+++ tests/datastore/datastore.py
@@ -0,0 +1,177 @@
+"""
+Tests for the dataproxy/datastore/whatever that can test the common edge cases
+and should exercise the python library at the same time.
+"""
+import uuid
+import os, sys
+import unittest
+import json
+sys.path.append(os.path.abspath( os.path.join(os.path.abspath(__file__), '../../../scraperlibs/python/')))
+try:
+    import scraperwiki
+except Exception, e:
+    print '*' * 80    
+    print '* Make sure the folder containing scraperlibs/python is in your $PYTHONPATH'
+    print '* Suggest:',  os.path.abspath( os.path.join(os.path.abspath(__file__), '../../../scraperlibs/python/'))
+    print '*' * 80
+    print e
+    sys.exit(0)
+
+resource_dir = None
+
+def title(s):
+    print '\n%s' % s
+    print '-' * len(s)
+
+def cleanup(name):
+    p = os.path.join(resource_dir,name)
+    try:
+        os.unlink( os.path.join(p, 'defaultdb.sqlite'))
+        os.rmdir( p )
+    except:
+        pass    
+
+def update_settings_for_name(settings,name):
+    import hashlib
+    secret_key = '%s%s' % (name, settings['secret'],)
+    settings['verification_key'] = hashlib.sha256(secret_key).hexdigest()  
+    del settings['secret']
+
+
+##############################################################################
+#
+# Tests - Start of the tests, all should extend DataStoreTester so that setUp
+# and tearDown happen correctly.
+#        
+##############################################################################
+
+
+class DataStoreTester(unittest.TestCase):
+    """
+    Create a datastore connection for the tests to use
+    """
+    def setUp(self):
+        scraperwiki.logfd = sys.stdout
+        self.settings = json.loads( open( os.path.join(os.path.dirname( __file__ ), "dev_test_settings.json") ).read() )        
+        self.settings['scrapername'], self.settings['runid'] = self.random_details()
+        update_settings_for_name(self.settings,self.settings['scrapername'])
+        scraperwiki.datastore.create( **self.settings )
+        
+    def random_details(self):
+        return 'x_' + str(uuid.uuid4()), str(uuid.uuid4()), 
+        
+    def tearDown(self):
+        global resource_dir
+        if resource_dir:
+            cleanup(self.settings['scrapername'])
+        else:
+            print 'Should delete the resourcedir directory called %s' % self.settings['scrapername']
+        scraperwiki.datastore.close()
+        
+        
+        
+class BasicDataProxyTests( DataStoreTester ):
+    """
+    Basic tests that the data proxy is working and allowing us to query data
+    """
+    
+    def test_simple_create_and_check(self):
+        title('test_simple_create_and_check')
+        # Check we can save and get the right count back
+        scraperwiki.sqlite.save(['id'], {'id':1})
+        x = scraperwiki.sqlite.execute('select count(*) from swdata')
+        self.assertEqual( x['data'][0][0], 1)
+        
+        
+    def test_simple_create_and_check_custom_table(self):
+        title('test_simple_create_and_check_custom_table')
+        # Check we can save to a named table and get the right count back        
+        scraperwiki.sqlite.save(['id'], {'id':1}, table_name='test table')
+        x = scraperwiki.sqlite.execute('select count(*) from `test table`')
+        self.assertEqual( x['data'][0][0], 1)        
+
+
+    def test_simple_create_and_check_custom_table_fail(self):
+        title('test_simple_create_and_check_custom_table_fail')
+        # Check we can save to a named table and failed to get data back when 
+        # we access swdata
+        scraperwiki.sqlite.save(['id'], {'id':1}, table_name='test table')
+        try:
+            x = scraperwiki.sqlite.execute('select count(*) from `swdata`')
+            self.fail("Found the custom table magically")
+        except AssertionError, e:
+            self.fail(e)
+        except:
+            pass # We expect an error so we can ignore it
+        
+
+    def test_attach_denied(self):
+        title('test_attach')
+        settings = json.loads( open( os.path.join(os.path.dirname( __file__ ), "dev_test_settings.json") ).read() )        
+        settings['scrapername'], settings['runid'] = self.random_details()
+        update_settings_for_name(settings,settings['scrapername'])        
+        attach_to = settings['scrapername']
+        scraperwiki.datastore.create( **settings )
+        print scraperwiki.sqlite.save(['id'], {'id':1}, table_name='test')
+        scraperwiki.datastore.close()
+
+        settings = json.loads( open( os.path.join(os.path.dirname( __file__ ), "dev_test_settings.json") ).read() )                
+        settings['scrapername'], settings['runid'] = self.random_details()
+        update_settings_for_name(settings,settings['scrapername'])       
+        scraperwiki.datastore.create( **settings )
+        try:
+            scraperwiki.sqlite.attach(attach_to,attach_to)         
+            scraperwiki.sqlite.select('* from `%s`.test' % attach_to)
+        except Exception, e:
+            # We expect to fail here so if we succeeded then we should fail
+            self.fail(e)
+        cleanup(attach_to)
+
+    def test_create_download_sqlite(self):
+        import base64
+        
+        title('test_create_download_sqlite')
+        scraperwiki.sqlite.save(['id'], {'id':1}, table_name='test table')
+        initsqlitedata = scraperwiki.datastore.request({"maincommand":"sqlitecommand", "command":"downloadsqlitefile", "seek":0, "length":0})
+        if "filesize" not in initsqlitedata:
+            print str(initsqlitedata)
+            return
+        filesize = initsqlitedata['filesize']         
+        size = 0
+        memblock=100000
+        for offset in range(0, filesize, memblock):
+            sqlitedata = scraperwiki.datastore.request({"maincommand":"sqlitecommand", "command":"downloadsqlitefile", "seek":offset, "length":memblock})
+            content = sqlitedata.get("content")
+            if sqlitedata.get("encoding") == "base64":
+                content = base64.decodestring(content)            
+            print len(content), sqlitedata.get("length")
+            self.failUnless( len(content) == sqlitedata.get("length") )
+        
+        
+    def test_create_download_csv(self):
+        pass
+
+        
+##############################################################################
+#
+# Main - see if we can guess where the resourcedir is and warn the user, so 
+# they can check as we will be deleting folders. May be better to check that
+# we are running in dev.
+#        
+##############################################################################
+
+if __name__ == '__main__':
+    p = os.path.abspath(__file__)
+    p = os.path.abspath( os.path.join(p, '../../../resourcedir/') )
+    if os.path.exists(p):
+        resource_dir = p
+        print ''
+        print '*' * 80
+        print 'Guessed that resourcedir is %s \nand created folders will be deleted from there.' % resource_dir
+        print '\nIs this okay?'
+        s = raw_input('[y/N]--> ')
+        if not s or s.lower() != 'y':
+            sys.exit(0)
+        print '\nRunning tests'
+        print '=' * 13
+    unittest.main()
---
+++ web/codewiki/models/vault.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+PLAN_TYPES = (
+    ('individual', 'Individual'),
+    ('corporate', 'Corporate'),    
+)
+
+# Multiple instances per user are now allowed
+class Vault(models.Model):
+    
+    user = models.ForeignKey(User, related_name='vaults')
+    name = models.CharField(max_length=64, blank=True)    
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    plan = models.CharField(max_length=32, choices=PLAN_TYPES)    
+
+    # A list of the members who can access this vault.  This is 
+    # distinct from the owner (self.user) of the vault.
+    members = models.ManyToManyField(User, related_name='vault_membership')
+
+    def get_code_objects(self):
+        from codewiki.models import UserCodeRole, Code                
+        return Code.objects.filter(vault=self).exclude(privacy_status='deleted')
+
+    def add_user_rights(self, user ):
+        """
+        A new user has been added to the vault, make sure they can access all of 
+        the code objects.
+        """
+        from codewiki.models import UserCodeRole, Code        
+        role = 'editor'
+        if user == self.user:
+            role = 'owner'
+        for code_object in self.get_code_objects():
+            UserCodeRole(code=code_object, user=user,role='editor').save()
+            
+        
+    def remove_user_rights(self, user ):
+        """
+        A user has been removed from the vault, make sure they can access none of 
+        the code objects.
+        """
+        from codewiki.models import UserCodeRole, Code                
+        for code_object in self.get_code_objects():
+            UserCodeRole.objects.filter(code=code_object, user=user).all().delete()
+        
+        
+    def update_access_rights(self):
+        """
+        A code_object has been added to the vault, make sure the UserCodeRoles
+        are correct.
+        """
+        from codewiki.models import UserCodeRole, Code                
+        for code_object in self.get_code_objects():
+            UserCodeRole.objects.filter(code=code_object).all().delete()
+            users = list(self.members.all())
+            try:
+                users.remove( self.user )
+            except ValueError:
+                pass
+                
+            UserCodeRole(code=code_object, user=self.user,role='owner').save()
+            for u in users:
+                UserCodeRole(code=code_object, user=u,role='editor').save()
+
+
+
+    def __unicode__(self):
+        return "%s' %s vault (created on %s)" % (self.user.username, self.plan, self.name)
+
+    class Meta:
+        app_label = 'codewiki'
+        ordering = ['-name']
+
+
